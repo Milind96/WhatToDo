ba44b90eef736280f71f3ed4a51b2571548cd1f7
Milind96 <mlndshelat@gmail.com>
2020-01-23 19:15:52 +0530

    initial commit

---
+++ WhatToDo/WhatToDo/WhatToDoVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WhatToDo
+//
+//  Created by milind shelat on 23/01/20.
+//  Copyright © 2020 milind shelat. All rights reserved.
+//
+
+import UIKit
+
+class WhatToDoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
